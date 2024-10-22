aa833682ea7dad5b967bf3fa7edef1215044cdaa
Thawat-2061 <65011212061@msu.ac.th>
2024-10-22 16:56:19 +0700

    BBBB

--- api/user.ts
+++ api/user.ts
@@ -27,3 +27,26 @@ router.get("/:userId", (req, res) => {
     res.json(result);
   });
 });
+
+router.get("/show/:SenderID", (req, res) => {
+  const userId = req.params.SenderID; // รับค่า UserID จาก URL parameter
+
+  // SQL query เพื่อค้นหาผู้ใช้ทุกคนยกเว้นตัวเอง
+  const sql = "SELECT * FROM deliveryorders WHERE SenderID = ?";
+
+  // เรียกใช้การ query ไปที่ฐานข้อมูล โดยส่ง UserID ไปใน array แทน
+  conn.query(sql, [userId], (err, result) => {
+    if (err) {
+      // ส่ง error 500 หากเกิดข้อผิดพลาดจากการ query
+      return res.status(500).json({ error: err.message });
+    }
+
+    // ตรวจสอบว่าพบข้อมูลผู้ใช้หรือไม่
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No user found" });
+    }
+
+    // ส่งข้อมูลที่พบกลับไปให้ผู้เรียก API
+    res.json(result);
+  });
+});
